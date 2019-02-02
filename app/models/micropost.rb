327cb0275aedd4de492ca658b2bc0ab86c59a80f
nakaimasai <nakai1234y@gmail.com>
2019-02-02 15:39:53 +0900

    navrai

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
   validate  :picture_size
   has_many :sankas, dependent: :destroy
