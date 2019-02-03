e33c3b8940e23d3c6a3a309c397c25785385ab29
nakaimasai <nakai1234y@gmail.com>
2019-02-03 17:52:52 +0900

    ippo

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,25 @@ User.create!(name:  "Example User",
              password_confirmation: "qwertyu",
              admin: true)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+#99.times do |n|
+  #name  = Faker::Name.name
+  #email = "example-#{n+1}@railstutorial.org"
+  #password = "password"
+  #User.create!(name:  name,
+               #email: email,
+               #password:              password,
+               #password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  title = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content, title: title) }
-end
+#users = User.order(:created_at).take(6)
+#50.times do
+  #content = Faker::Lorem.sentence(5)
+  #title = Faker::Lorem.sentence(5)
+  #users.each { |user| user.microposts.create!(content: content, title: title) }
+#end
 
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+#users = User.all
+#following = users[2..50]
+#followers = users[3..40]
+#following.each { |followed| user.follow(followed) }
+#followers.each { |follower| follower.follow(user) }
